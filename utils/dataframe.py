492809e24d8e6c1820e4ad1841e7855c110ea686
congruiyS2023 <congruiy@andrew.cmu.edu>
2023-04-27 23:15:29 -0700

    Include counts in write_unique_values_to_file

--- utils/dataframe.py
+++ utils/dataframe.py
@@ -1,24 +1,22 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
 
-def write_unique_values_to_file(data: pd.DataFrame, column: str, file_name: str, sort: bool = True):
+def write_unique_values_to_file(data: pd.DataFrame, column: str, file_name: str):
     """
     Write unique values of a column in a dataframe to a file
     :param data: dataframe
     :param column: column name
     :param file_name: file to write
-    :param sort: if to sort the unique values
     :return: None
     """
-    if sort:
-        unique_values = sorted(data[column].unique())
-    else:
-        unique_values = data[column].unique()
+    values_np = np.array(data[column])
+    unique_values, value_counts = np.unique(values_np, return_counts=True)
     with open(file_name, 'w') as f:
-        for value in unique_values:
-            f.write(str(value) + '\n')
+        for value, count in zip(unique_values, value_counts):
+            f.write(value + '\t' + str(count) + '\n')
 
 
 def plot_hist(data: pd.DataFrame, column: str, kde: bool, title: str, x_label: str, y_label: str, file_name: str):
